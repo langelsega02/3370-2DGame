eab22bc5202b89fd04337d65460bd61f0e223954
melanieloredo <melanie.loredo01@utrgv.edu>
2025-05-12 00:31:28 -0500

    working menu & howto scenes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void Instructions()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
 }
