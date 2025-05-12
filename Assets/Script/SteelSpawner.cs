99d7c4c5e47876570527be6f69c0fc3a5b433263
langelsega02 <langelsega02@gmail.com>
2025-05-12 01:34:38 -0500

    Added coin animation

---
+++ Assets/Script/SteelSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteelSpawner : MonoBehaviour
+{
+    public float spawnRate = 5f; // faster spawn for city look
+    private float timer = 0f;
+    public GameObject steelPrefab;
+    public GameObject coinPrefab;
+    public float minY = 0f;
+    public float maxY = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBuilding();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnBuilding();
+            timer = 0;
+        }
+    }
+
+    void SpawnBuilding()
+    {
+        int randomLength = Random.Range(1, 5);
+        int randomSpawn = Random.Range(1, 4);
+
+        float y = Random.Range(minY, maxY);
+        for (int i = 0; i < randomLength; i++ )
+        {
+            Instantiate(steelPrefab, new Vector3(transform.position.x+i, y, 0), Quaternion.identity);
+        }
+        if (randomSpawn == 4)
+        {
+            Instantiate(coinPrefab, new Vector3(transform.position.x + (randomLength / 2), y, 0), Quaternion.identity);
+        }
+
+    }
+}
